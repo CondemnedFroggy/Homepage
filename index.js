713c9d619796c735d8d89498660e9fd804d51dde
CondemnedFroggy <54013431+CondemnedFroggy@users.noreply.github.com>
2019-10-14 18:37:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,27 +31,3 @@ setTimeout(function(){
       $("#loading").css("display","none");
     },800);
 },1450);
-
-
-countdown(function(){
-  var countDownDate = new Date("Oct 26, 2019 07:00:00").getTime();
-
-  var countdownfunction = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="countdown"
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-      clearInterval(countdownfunction);
-      document.getElementById("countdown").innerHTML = "HET OUT THERE AND FIGHT";
-    }
-  }, 1000);
-});
